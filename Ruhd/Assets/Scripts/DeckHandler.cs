51873fd713e9384d01f4cc650ebf60f23139a005
Alex Denford <pantherNZ@users.noreply.github.com>
2023-09-16 20:28:30 -0700

    Game over screen visuals

--- Ruhd/Assets/Scripts/DeckHandler.cs
+++ Ruhd/Assets/Scripts/DeckHandler.cs
@@ -118,6 +118,8 @@ public class DeckHandler : EventReceiverInstance
                         EventSystem.Instance.TriggerEvent( new GameOverEvent() );
                     }
                 }
+
+                EventSystem.Instance.TriggerEvent( new GameOverEvent() );
             }
 
             selectedCardFromSlot = null;
--- Ruhd/Assets/Scripts/GameOverUI.cs
+++ Ruhd/Assets/Scripts/GameOverUI.cs
@@ -10,6 +10,7 @@ public class GameOverUI : EventReceiverInstance
     [SerializeField] VerticalLayoutGroup resultsLayoutgroupRef;
     [SerializeField] GameObject resultsPanelRef;
     [SerializeField] GameObject playerResultPrefab;
+    [SerializeField] CanvasGroup showPanelButtonRef;
     [SerializeField] float panelScoresDelaySec = 1.0f;
     [SerializeField] float panelInterpXOffset = -250.0f;
     [SerializeField] float panelInterpTimeSec = 0.5f;
@@ -20,6 +21,7 @@ public class GameOverUI : EventReceiverInstance
     {
         base.Start();
 
+        showPanelButtonRef.SetVisibility( false );
     }
     
     public void TogglePanel()
@@ -28,16 +30,20 @@ public class GameOverUI : EventReceiverInstance
 
         if( resultsPanelRef.activeSelf )
         {
-            this.FadeFromTransparent( resultsPanelRef, panelFadeTimeSec );
+            this.FadeFromTransparent( resultsPanelRef, panelFadeTimeSec, Utility.Easing.Quintic.In );
             resultsPanelRef.transform.localPosition = new Vector3( panelInterpXOffset, 0.0f, 0.0f );
             this.InterpolatePosition( resultsPanelRef.transform, Vector3.zero, panelInterpTimeSec, true, Utility.Easing.Quintic.In );
             resultsPanelRef.transform.localScale = new Vector3( 0.0f, 1.0f, 1.0f );
             this.InterpolateScale( resultsPanelRef.transform, Vector3.one, panelInterpTimeSec, Utility.Easing.Quintic.In );
+            if( showPanelButtonRef.IsVisible() )
+                this.FadeToTransparent( showPanelButtonRef, panelInterpTimeSec, Utility.Easing.Quintic.Out );
         }
         else
         {
-            this.FadeToTransparent( resultsPanelRef, panelFadeTimeSec, disableObjectOnFinish: true );
-            this.InterpolatePosition( resultsPanelRef.transform, new Vector3( panelInterpXOffset, 0.0f, 0.0f ), panelInterpTimeSec / 2.0f, true );
+            this.FadeToTransparent( resultsPanelRef, panelFadeTimeSec, Utility.Easing.Quintic.In, true );
+            this.InterpolatePosition( resultsPanelRef.transform, new Vector3( panelInterpXOffset, 0.0f, 0.0f ), panelInterpTimeSec, true, Utility.Easing.Quintic.In );
+            this.InterpolateScale( resultsPanelRef.transform, new Vector3( 0.0f, 1.0f, 1.0f ), panelInterpTimeSec, Utility.Easing.Quintic.In );
+            this.FadeFromTransparent( showPanelButtonRef, panelInterpTimeSec, Utility.Easing.Quintic.In );
         }
     }
 
